2c4fca558f1c072d210d2828d04c043ff98d87c5
Himanshu jethva <himanshujethva9524@gmail.com>
2025-07-25 14:39:35 +0530

    Add files via upload

---
+++ 23-7/task/task 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    char name[] = "H B J ";
+    char birth_date[] = "15-11-2003";
+    int age = 21;
+    char address[] = "123 Main st, al tower, dubai";
+
+    printf("\n Name: %s", name);
+    printf("\n Birth Date: %s", birth_date);
+    printf("\n Age: %d years", age);
+    printf("\n Address: %s", address);
+    
+    getch();
+}
+
