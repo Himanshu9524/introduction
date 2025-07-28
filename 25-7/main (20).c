cc0c027c26f5b43f7acc1b326b85dffa9eb83416
Himanshu jethva <himanshujethva9524@gmail.com>
2025-07-28 15:01:31 +0530

    Add files via upload

---
+++ 25-7/main (20).c
@@ -0,0 +1,18 @@
+//do while loop
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i=10;
+   
+   do
+   {
+       // inciatization
+      printf("\n %d",i); 
+      i--;
+   }
+      //conditoin
+   while(i>=1);
+    getch();
+}
---
+++ 25-7/main (21).c
@@ -0,0 +1,18 @@
+//do while loop
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i=3;
+   
+   do
+   {
+       // intialization
+      printf("\n %d",i); 
+      i++;
+   }
+      //conditoin
+   while(i<=5);
+    getch();
+}
